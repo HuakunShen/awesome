72b1005a1f9017515fa1c28da2c795f272930c10
Huakun Shen <huakun.shen@huakunshen.com>
2024-08-14 17:19:32 -0400

    💚 Fix CI

--- apps/web/src/lib/server/db.ts
+++ apps/web/src/lib/server/db.ts
@@ -1,13 +1,3 @@
 import { env } from "$env/dynamic/private"
-import { AwesomeListDao, AwesomeRepoDao, getAdminPocketBaseClient, IsAwesomeDao } from "db"
 
-const { PB_URL, PB_ADMIN_USERNAME, PB_ADMIN_PASSWORD } = env
-
-export const adminDBClient = await getAdminPocketBaseClient(
-	PB_URL,
-	PB_ADMIN_USERNAME,
-	PB_ADMIN_PASSWORD
-)
-export const awesomeListDao = new AwesomeListDao(adminDBClient)
-export const awesomeRepoDao = new AwesomeRepoDao(adminDBClient, awesomeListDao)
-export const isAwesomeDao = new IsAwesomeDao(adminDBClient)
+export { db } from "db"
--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,8 +1,9 @@
 import { error } from "@sveltejs/kit"
-import { awesomeListDao, isAwesomeDao } from "$lib/server/db"
+import { db } from "$lib/server/db"
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	const ret = await Promise.all([isAwesomeDao.getAll({}), awesomeListDao.getAll({})])
-	return { awesomeLists: ret[1], isAwesomes: ret[0] }
+	return { awesomeLists: await db.getAllAwesomeLists() }
+	// const ret = await Promise.all([isAwesomeDao.getAll({}), awesomeListDao.getAll({})])
+	// return { awesomeLists: ret[1], isAwesomes: ret[0] }
 }
